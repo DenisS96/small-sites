cf4330bc52543011d65de97c53be51b7dcce0510
DenisS96 <88380857+DenisS96@users.noreply.github.com>
2024-11-01 21:23:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -62,3 +62,29 @@ function doorzichtigeHeader(){
 }
 
 
+
+
+document.querySelector('.quote-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Formulier niet echt indienen, voorbeelddoeleinden
+    alert('Bedankt voor je aanvraag! We nemen snel contact met je op.');
+  });
+  
+
+
+
+  const reviews = document.querySelectorAll('.review-card');
+let currentReview = 0;
+
+function showNextReview() {
+  reviews[currentReview].style.display = 'none';
+  currentReview = (currentReview + 1) % reviews.length;
+  reviews[currentReview].style.display = 'block';
+}
+
+// Start de carrousel om elke 3 seconden van review te wisselen
+setInterval(showNextReview, 3000);
+
+// Zorg ervoor dat alleen de eerste review zichtbaar is bij het laden van de pagina
+reviews.forEach((review, index) => {
+  if (index !== 0) review.style.display = 'none';
+});
